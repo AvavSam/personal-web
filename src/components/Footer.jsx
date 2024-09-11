7fe3f23acfce6a879292411e83f4f0a196436abd
AvavSam <avav2608005@gmail.com>
2024-09-11 14:51:01 +0800

    chore: add meta tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="mt-8 text-center text-gray-500">
+    <footer className="mt-8 text-center text-gray-700">
       <p>&copy; 2024 🤍 Avav Abdillah Sam</p>
     </footer>
   );
